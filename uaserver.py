d67105ba1d3ad49bf6503d2c2bb36fe392e16e34
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2018-06-24 14:18:24 +0200

    redoing

--- uaserver.py
+++ uaserver.py
@@ -5,20 +5,16 @@
 import socketserver
 import sys
 import os
-from uaclient import CONFIGHandler
+from xml.sax import make_parser
+from uaclient import CONFIGHandler#, Configurator
 
-try:
-    _, CONFIG = sys.argv
-
-except ValueError:
-    sys.exit('Usage: python3 uaserver.py config')
-
+CONFIG = ""
 ########################INICIO#################
 
 class Configurator(CONFIGHandler):
 
     """Configurator."""
-    def __init__(self, metodo, option):
+    def __init__(self):
         """initialize selfs"""
         parser = make_parser()
         self.cHandler = CONFIGHandler()
@@ -28,7 +24,7 @@ class Configurator(CONFIGHandler):
         self.login = self.cHandler.attributs['account']['username']
         self.password = self.cHandler.attributs['account']['passwd']
         self.ip = self.cHandler.attributs['uaserver']['ip']
-        self.port = self.cHandler.attributs['uaserver']['puerto']
+        self.port = int(self.cHandler.attributs['uaserver']['puerto'])
         self.rtpport = self.cHandler.attributs['rtpaudio']['puerto']
         self.PX_SERVER = self.cHandler.attributs['regproxy']['ip']
         self.PX_PORT = int(self.cHandler.attributs['regproxy']['puerto'])
@@ -38,22 +34,32 @@ class Configurator(CONFIGHandler):
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """Echo server class."""
+    print('popop')
 
     def check_method(self, method, protocol, sip):
         """function for check the method."""
+        config2 = Configurator()
         methods = ['INVITE', 'ACK', 'BYE']
         data_send = ""
         if method in methods:
-            if (protocol == 'SIP/2.0\r\n\r\n') and (sip[0:4] == 'sip:'):
+            if (protocol == 'SIP/2.0') and (sip[0:4] == 'sip:'):
                 if method == 'INVITE':
+                    print('loase')
                     data_send = ("SIP/2.0 100 Trying\r\n\r\n" +
                                  "SIP/2.0 180 Ringing\r\n\r\n" +
                                  "SIP/2.0 200 OK\r\n\r\n")
+                    data_send+= ('Content-Type: application/sdp\r\n\r\n' +
+                                'v=0\r\n' +
+                                'o={} {}\r\n'.format(config.login, config.ip) +
+                                's=misesion\r\n' +
+                                't=0\r\n' +
+                                'v=0\r\n' +
+                                'm=audio {} RTP\r\n\r\n'.format(config.rtpport))
                 elif method == 'BYE':
                     data_send = "SIP/2.0 200 OK\r\n\r\n"
                 elif method == 'ACK':
-                    os.system('mp32rtp -i 127.0.0.1 -p 23032 < ' +
-                              CANCION)
+                    os.system('mp32rtp -i {} -p {} < {}'.format(config.ip,
+                    config.rtpport, config.audio_path))
             else:
                 data_send = "SIP/2.0 400 Bad Request\r\n\r\n"
         else:
@@ -63,12 +69,25 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         """handler server."""
         data = self.rfile.read().decode('utf-8')
-        metodo, sip_address, protocol = data.split(' ')
+        data = data.split('\r\n')
+        metodo, sip_address, protocol = data[0].split(' ')
+        print(metodo)
         self.check_method(metodo, protocol, sip_address)
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
+    try:
+        CONFIG = sys.argv[1]
 
-    serv = socketserver.UDPServer((IP, PORT), EchoHandler)
+    except ValueError:
+        sys.exit('Usage: python3 uaserver.py config')
+
+    config = Configurator()
+
+    serv = socketserver.UDPServer((config.ip, config.port), EchoHandler)
     print("Listening...")
-    serv.serve_forever()
+    print(config.ip,config.port)
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("\r\nFinalizado servidor")
